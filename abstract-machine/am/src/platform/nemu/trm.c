ca5f4878dfd7a616f53d432081300d05d9c4b7be
tracer-ics2022 <tracer@njuics.org>
2022-10-24 23:33:21 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:33:21 up  1:32,  3 users,  load average: 0.20, 0.24, 0.29

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -4,17 +4,17 @@
 extern char _heap_start;
 int main(const char *args);
 
-Area heap = RANGE(&_heap_start, PMEM_END);
+Area heap = RANGE(&_heap_start, PMEM_END);//指示堆区的起始和末尾
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
-void putch(char ch) {
+void putch(char ch) {//输出一个字符
   outb(SERIAL_PORT, ch);
 }
 
-void halt(int code) {
+void halt(int code) {//结束程序运行
   nemu_trap(code);
 
   // should not reach here
