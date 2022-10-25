0a620fe11763ad9913b6be406111bd0ed1849a86
tracer-ics2022 <tracer@njuics.org>
2022-10-25 17:27:24 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:27:24 up  1:21,  3 users,  load average: 1.21, 1.00, 0.83

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -29,6 +29,8 @@ CPU_state cpu = {};//创建一个cpu
 // word_t gpr[32];
 //  vaddr_t pc;//PC
 
+char iringbuf[128];
+int curIringBuf = 0;
 uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
@@ -41,6 +43,13 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   //itrace
+  IFDEF(CONFIG_ITRACE, sprintf(iringbuf + curIringBuf,"%s",_this->logbuf));
+
+  puts(iringbuf);
+  curIringBuf = strlen(_this->logbuf) + curIringBuf;
+  curIringBuf %= 128;
+  //add
+  
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
   if(!check_wp())
