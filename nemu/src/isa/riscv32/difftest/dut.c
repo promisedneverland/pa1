da5c04e3010d86de8a964cce3b1c658b5544c586
tracer-ics2022 <tracer@njuics.org>
2022-10-29 16:50:28 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:50:28 up 49 min,  3 users,  load average: 0.37, 0.43, 0.45

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,6 +19,11 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   
+  for(int i = 0 ; i <= 31 ; i++)
+  {
+    if(ref_r->gpr[i] != cpu.gpr[i])
+      return false;
+  }
   return true;
 }
 
