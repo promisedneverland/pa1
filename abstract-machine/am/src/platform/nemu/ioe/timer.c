9b11a769f3004b6dea26af3d0b8ade09dca5a6e3
tracer-ics2022 <tracer@njuics.org>
2022-10-30 20:06:47 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:47 up  4:52, 16 users,  load average: 0.59, 0.52, 0.66

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -7,9 +7,9 @@ void __am_timer_init() {
 
 //RTC_ADDR处的内存到底是怎么更新的
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // uptime->us = inl(RTC_ADDR + 4);
-  // uptime->us <<= 32;
-  // uptime->us |= inl(RTC_ADDR);
+  uptime->us = inl(RTC_ADDR + 4);
+  uptime->us <<= 32;
+  uptime->us |= inl(RTC_ADDR);
   // uptime->us = paddr_read(RTC_ADDR, 4);
   // uptime->us <<= 32;
   // uptime->us |= paddr_read(RTC_ADDR + 4, 4);
