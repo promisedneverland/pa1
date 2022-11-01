0c96b08e35b5a410710b607d2e88eba9b9698f33
tracer-ics2022 <tracer@njuics.org>
2022-11-02 00:01:45 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:01:45 up  9:22,  7 users,  load average: 0.46, 0.72, 0.85

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -27,14 +27,14 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   
   int w = inw(VGACTL_ADDR + 2);  
-  printf("x = %d, y = %d, w = %d, h = %d,pixel[0] = %d \n",ctl->x,ctl->y,ctl->w,ctl->h,*((uint32_t *)ctl->pixels));
+  //printf("x = %d, y = %d, w = %d, h = %d,pixel[0] = %d \n",ctl->x,ctl->y,ctl->w,ctl->h,*((uint32_t *)ctl->pixels));
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (int i = ctl->y; i < ctl->y + ctl->h ; i++)
   {
     for(int j = ctl->x; j < ctl->x + ctl->w; j++)
     {
       fb[j + i * w] = ((uint32_t *)ctl->pixels)[ j - ctl->x + (i - ctl->y)*(ctl->w)];
-      printf("pixel = %d\n",fb[j + i * w]);
+      //printf("pixel = %d\n",fb[j + i * w]);
     }
       
   }
