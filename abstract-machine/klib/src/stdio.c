86fbcd8b7a3f69890742301a263937a9c421eef8
tracer-ics2022 <tracer@njuics.org>
2022-11-01 17:29:47 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:29:47 up  2:50,  6 users,  load average: 0.91, 0.71, 0.72

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -126,6 +126,7 @@ void itoa(long long integer, char* out, int base)//only base = 10 is valid
   out[charnum] = 0;
 }
 int sprintf(char *out, const char *fmt, ...) {
+  putch('h');
   int charout = 0;
   assert(0);
   PRINTF_HANDLE(charout,out);
