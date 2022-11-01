53b2f747c677256e02c690f164b6b95a6e5875d2
tracer-ics2022 <tracer@njuics.org>
2022-11-01 22:12:09 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:12:09 up  7:33,  7 users,  load average: 1.65, 1.08, 0.83

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,8 +5,8 @@
 
 void __am_gpu_init() {
   int i;
-  int w = inw(FB_ADDR + 2);  // TODO: get the correct width
-  int h = inw(FB_ADDR);  // TODO: get the correct height
+  int w = inw(FB_ADDR + 2)*32;  // TODO: get the correct width
+  int h = inw(FB_ADDR)*32;  // TODO: get the correct height
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   //同步开
@@ -16,7 +16,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = inw(FB_ADDR + 2), .height = inw(FB_ADDR),
+    .width = inw(FB_ADDR + 2)*32, .height = inw(FB_ADDR)*32,
     .vmemsz = inw(FB_ADDR + 2) * inw(FB_ADDR) * sizeof(uint32_t),
   };
 }
