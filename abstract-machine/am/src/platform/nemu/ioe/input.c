b43bd53444f0b9f4929d6db2105167401428b7e5
tracer-ics2022 <tracer@njuics.org>
2022-11-01 20:12:00 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:12:00 up  5:32,  7 users,  load average: 1.06, 0.87, 0.69

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,9 +5,12 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   //kbd->keycode = 0;
-  if(KBD_ADDR == 0)
-    putch('?');
+  putch(inb(KBD_ADDR));
+   putch(inb(KBD_ADDR+2));
+    putch(inb(KBD_ADDR+3));
+     putch(inb(KBD_ADDR+4));
   kbd->keydown = 1;
+  // if(kbd->keydown == 0)
   kbd->keycode = inl(KBD_ADDR);
   // send_key(keycode);
   // kbd->keydown = (kbd->keycode) & KEYDOWN_MASK;
