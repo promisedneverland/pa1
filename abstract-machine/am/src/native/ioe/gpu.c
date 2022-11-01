e3168e748dad05ffcc867038fc1550b663ec9c88
tracer-ics2022 <tracer@njuics.org>
2022-11-01 23:42:36 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:42:36 up  9:03,  7 users,  load average: 1.18, 0.98, 0.90

--- abstract-machine/am/src/native/ioe/gpu.c
+++ abstract-machine/am/src/native/ioe/gpu.c
@@ -56,6 +56,8 @@ void __am_gpu_status(AM_GPU_STATUS_T *stat) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  printf("x = %d, y = %d, w = %d, h = %d,pixel[0] = %d \n",ctl->x,ctl->y,ctl->w,ctl->h,*((uint32_t *)ctl->pixels));
+
   if (w == 0 || h == 0) return;
   feclearexcept(-1);
   SDL_Surface *s = SDL_CreateRGBSurfaceFrom(ctl->pixels, w, h, 32, w * sizeof(uint32_t),
