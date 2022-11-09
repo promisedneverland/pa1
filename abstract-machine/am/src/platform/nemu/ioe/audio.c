ada44e5a506e226b208f3a6ada847a8466ba0801
tracer-ics2022 <tracer@njuics.org>
2022-11-09 20:51:15 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:51:15 up  2:02,  4 users,  load average: 0.72, 0.69, 0.65

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -26,11 +26,14 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_FREQ_ADDR,ctrl->freq);
   outl(AUDIO_CHANNELS_ADDR,ctrl->channels);
   outl(AUDIO_SAMPLES_ADDR,ctrl->samples);
+  outl(AUDIO_INIT_ADDR,1);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = inl(AUDIO_COUNT_ADDR);
 }
 
+//写入nemu
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+  
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -31,20 +31,19 @@ enum {
 //流缓冲区
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
-void init_sdl_audio()
-{
-  SDL_AudioSpec s = {};
-  s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
-  s.userdata = NULL;        // 不使用
-  //todo
-  SDL_InitSubSystem(SDL_INIT_AUDIO);
-  SDL_OpenAudio(&s, NULL);
-  SDL_PauseAudio(0);
-}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
   if(audio_base[reg_init] == 1)
   {
-    
+    SDL_AudioSpec s = {};
+    s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+    s.userdata = NULL;        // 不使用
+    s.freq = audio_base[reg_freq];
+    s.channels = audio_base[reg_channels];
+    s.samples = audio_base[reg_samples];
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
     audio_base[reg_init] = 0;
   }
 }
