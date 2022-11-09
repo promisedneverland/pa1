91e17c5705680b7415c446d1b6531d01db4188eb
tracer-ics2022 <tracer@njuics.org>
2022-11-09 15:45:47 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:45:47 up  3:20,  3 users,  load average: 0.18, 0.44, 0.42

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -20,11 +20,14 @@ Context* __am_irq_handle(Context *c) {
 
 extern void __am_asm_trap(void);
 
+
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
+  //设置异常入口地址
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler
+  //注册事件处理回调函数，由nanos提供
   user_handler = handler;
 
   return true;
@@ -34,6 +37,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   return NULL;
 }
 
+//自限指令
 void yield() {
   asm volatile("li a7, -1; ecall");
 }
