0ec3b7f39cbcff369d422d5c79dc03654585650a
tracer-ics2022 <tracer@njuics.org>
2022-11-20 19:02:40 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:02:40 up  4:18,  4 users,  load average: 1.96, 1.53, 1.42

--- nemu/src/engine/interpreter/hostcall.c
+++ nemu/src/engine/interpreter/hostcall.c
@@ -14,7 +14,7 @@
 ***************************************************************************************/
 
 #include <utils.h>
-#include <cpu/ifetch.h>
+// #include <cpu/ifetch.h>
 #include <isa.h>
 #include <cpu/difftest.h>
 
