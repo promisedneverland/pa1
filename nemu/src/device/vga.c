2afd8c5d80d639a7977c261d4ff8bc82ad7ed29c
tracer-ics2022 <tracer@njuics.org>
2022-11-20 16:38:36 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:38:36 up  1:54,  4 users,  load average: 1.50, 1.50, 1.39

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -22,7 +22,7 @@
 
 //宽高查询
 static uint32_t screen_width() {
-  return MUXDEF(CONFIG_TARGET_AM, io_read(AM_GPU_CONFIG).width, SCREEN_W);
+  return MUXDEF(CONFIG_TARGET_AM, io_read(AM_GPU_CONFIG).width, 100);
 }
 
 static uint32_t screen_height() {
