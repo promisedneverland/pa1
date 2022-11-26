ea90be1efbd2650c32caa6c8920de7ed8776b598
tracer-ics2022 <tracer@njuics.org>
2022-11-26 17:25:11 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:25:11 up  4:15,  3 users,  load average: 1.44, 1.49, 1.44

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -34,7 +34,7 @@ static const uint32_t img [] = {
 static void restart() {//初始化寄存器
   /* Set the initial program counter. */
   cpu.pc = RESET_VECTOR;//让CPU从我们约定的内存位置开始执行客户程序
-
+  cpu.mstatus = 0x1800;
   /* 初始化 The zero register is always 0. */
   cpu.gpr[0] = 0;
 }
