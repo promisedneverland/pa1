dfd45ac828432edd09aba301bd2240eaaebff388
tracer-ics2022 <tracer@njuics.org>
2022-11-26 14:03:43 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:03:43 up 54 min,  2 users,  load average: 1.10, 0.67, 0.48

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -11,7 +11,7 @@ int main() {
   extern const char logo[];
   printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");
-  Log("Build time: %s, %s", __TIME__, __DATE__);
+  // Log("Build time: %s, %s", __TIME__, __DATE__);
 
 //   init_mm();
 
