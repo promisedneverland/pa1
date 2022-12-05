50930391ce1cd813dc5247cac856211cf7b471cf
tracer-ics2022 <tracer@njuics.org>
2022-12-05 19:02:29 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:02:29 up  3:04,  4 users,  load average: 1.55, 1.11, 0.83

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -48,7 +48,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
       // unsigned char* segment_loaded;
       // segment_loaded = malloc(phdr.p_filesz);
       printf("phdr.p_filesz = %x\n",phdr.p_filesz);
-      ramdisk_read((void*)phdr.p_vaddr,phdr.p_offset,phdr.p_filesz);
+      ramdisk_read((char*)phdr.p_vaddr,phdr.p_offset,phdr.p_filesz);
       // for(int j = 0; j < phdr.p_filesz ; j++)
       //   vaddr_write((uint32_t)1,1,(uint32_t)1);
       // free(segment_loaded);
