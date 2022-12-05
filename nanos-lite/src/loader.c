e80c0f1958ae57135b5e96734f862e012d85cb0c
tracer-ics2022 <tracer@njuics.org>
2022-12-05 16:21:51 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:21:51 up 24 min,  3 users,  load average: 1.01, 0.78, 0.48

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,6 +18,7 @@ char* elf;
 static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("init elf\n");
   ramdisk_read(elf,0,get_ramdisk_size());
+  printf("\n\n");
   printf("%s",elf);
   // assert(*(uint32_t *)elf->e_ident == 0xBadC0de);
 
