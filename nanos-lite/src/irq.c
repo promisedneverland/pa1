e1b78298aa2a409c6314dd7e3ed68a5f43845c10
tracer-ics2022 <tracer@njuics.org>
2022-12-06 23:34:51 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:34:51 up  1:05,  5 users,  load average: 1.26, 0.72, 0.79

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,11 +3,11 @@
 void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: 
-    {
-      printf("event = EVENT_YIELD\n");
-      break;
-    }
+    // case EVENT_YIELD: 
+    // {
+    //   printf("event = EVENT_YIELD\n");
+    //   break;
+    // }
     case EVENT_SYSCALL :
     {
       printf("event = EVENT_SYSCALL\n");
