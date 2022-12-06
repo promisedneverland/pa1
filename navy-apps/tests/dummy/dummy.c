68101ed4af53c7534f186628fdff90a286a245f3
tracer-ics2022 <tracer@njuics.org>
2022-12-06 23:33:05 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:33:05 up  1:03,  5 users,  load average: 0.80, 0.54, 0.76

--- navy-apps/tests/dummy/dummy.c
+++ navy-apps/tests/dummy/dummy.c
@@ -8,5 +8,7 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  return _syscall_(2, 0, 0, 0);
+  // printf("dummy\n");
+  return 0;
+  // return _syscall_(2, 0, 0, 0);
 }
