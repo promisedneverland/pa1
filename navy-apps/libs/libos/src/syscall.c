3cc1872f3c0c1ae2cc6d24744e6e034fdc83e1e5
tracer-ics2022 <tracer@njuics.org>
2022-12-07 19:22:24 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:22:24 up  1:08,  4 users,  load average: 1.39, 0.69, 0.45

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -69,11 +69,10 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  // if(_syscall_(SYS_sbrk, increment, 0, 0) == 0)
-  // {
-
-  // }
-  putch('$');
+  if(_syscall_(SYS_brk, increment, 0, 0) == 0)
+  {
+    return 1;
+  }
   return (void *)-1;
 }
 
