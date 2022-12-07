6a5343bf65a68495145ac80de8d1a372e6c35cce
tracer-ics2022 <tracer@njuics.org>
2022-12-07 14:57:15 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:57:15 up  2:04,  4 users,  load average: 1.29, 1.15, 1.10

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,8 +3,8 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    default: panic("Unhandled syddddscall ID = %d", a[0]);
   }
 }
