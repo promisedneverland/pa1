112c099ae903f16f9a637b51b71a1d005f2ac4bb
tracer-ics2022 <tracer@njuics.org>
2022-12-07 15:28:06 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:28:06 up  2:35,  4 users,  load average: 1.41, 1.52, 1.49

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,25 @@
 #include <common.h>
 #include "syscall.h"
+
+void sys_yield()
+{
+  yield();
+}
 void do_syscall(Context *c) {
   uintptr_t a[4];
-  a[0] = c->GPR1;
   
+  //通过宏GPR1从上下文c中获取用户进程之前设置好的系统调用参数
+  a[0] = c->GPR1;
+  a[0] = c->GPR1;
+  a[0] = c->GPR1;
+  a[0] = c->GPR1;
   switch (a[0]) {
+    case 1:
+    {
+      sys_yield();
+      c->GPRx = 0;
+      return;
+    }
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
