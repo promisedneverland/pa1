8c8955bdbd0161004351e1483943424ac082d38e
tracer-ics2022 <tracer@njuics.org>
2022-12-08 23:16:05 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:16:05 up  2:27,  4 users,  load average: 0.72, 0.70, 0.47

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -29,10 +29,10 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 //文件记录表，其下标为文件描述符
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin",  0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write },
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
-  [FD_EVENT]  = {"event",  0, 0, events_read, invalid_write},
+  [FD_STDIN]  = {"stdin",        0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout",       0, 0, invalid_read, serial_write },
+  [FD_STDERR] = {"stderr",       0, 0, invalid_read, invalid_write},
+  [FD_EVENT]  = {"/dev/events",  0, 0, events_read, invalid_write},
 #include "files.h"
 };
 
