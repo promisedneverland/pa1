1bc05f1faad62b79d23452642479b9e1ef7d42a4
tracer-ics2022 <tracer@njuics.org>
2022-12-08 22:01:10 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:01:10 up  1:12,  4 users,  load average: 1.19, 0.99, 0.87

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -18,8 +18,8 @@ void do_syscall(Context *c) {
   uintptr_t a[4];
   
   //通过宏GPR1从上下文c中获取用户进程之前设置好的系统调用参数
-  a[0] = c->GPR1;
-  a[1] = c->GPR2;
+  a[0] = c->GPR1; // 异常号
+  a[1] = c->GPR2; //第一个参数
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   // printf("strace : type = %d, gpr2 = %x, gpr3 = %x, gpr4 = %x\n\n", a[0],a[1],a[2],a[3]);
@@ -68,7 +68,7 @@ void do_syscall(Context *c) {
     }
     case SYS_gettimeofday:
     { 
-      c->GPRx = sys_gettimeofday((struct timeval*)a[0],(struct timezone*)a[1]); 
+      c->GPRx = sys_gettimeofday((struct timeval*)a[1],(struct timezone*)a[2]); 
       return ;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
