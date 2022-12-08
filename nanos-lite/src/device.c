cea34d8d727dcee2c87530cd082c75f58e2f5d52
tracer-ics2022 <tracer@njuics.org>
2022-12-08 21:55:14 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:55:14 up  1:06,  4 users,  load average: 0.66, 0.94, 0.83

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -40,7 +40,7 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz)
 {
   long long uptime;//微秒数
   ioe_read(AM_TIMER_UPTIME,&uptime);
-  // tv->tv_sec = uptime / 1000;
+  tv->tv_sec = uptime / 1000;
   // tv->tv_usec = uptime % 1000;
   return 0;
 }
