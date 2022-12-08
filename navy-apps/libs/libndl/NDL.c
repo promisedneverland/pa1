043a56be6f8489d01da2f5014c054e6e4dfda742
tracer-ics2022 <tracer@njuics.org>
2022-12-08 23:57:59 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:57:59 up  3:08,  4 users,  load average: 0.60, 0.62, 0.56

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -21,10 +21,10 @@ uint32_t NDL_GetTicks() {
 // 读出一条事件信息, 将其写入`buf`中, 最长写入`len`字节
 // 若读出了有效的事件, 函数返回1, 否则返回0
 int NDL_PollEvent(char *buf, int len) {
-  // FILE *fp = fopen("/dev/events", "r+");
-  // assert(fp);
+  FILE *fp = fopen("/dev/events", "r+");
+  assert(fp);
 
-  // fseek(fp, 0, SEEK_SET);
+  fseek(fp, 0, SEEK_SET);
   return 0;
 }
 
