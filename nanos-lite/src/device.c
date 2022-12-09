ae602e148cdb81a4e2a97aa35ce2b1741b74bdfa
tracer-ics2022 <tracer@njuics.org>
2022-12-09 16:07:54 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:07:54 up  3:46,  3 users,  load average: 0.97, 1.03, 1.08

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,7 +28,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
 
   assert(buf != NULL);
-  
+  //绝对不能io_read().keycode这么用 ？？？？？？
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   int keycode = ev.keycode;
   if(keycode != AM_KEY_NONE)
