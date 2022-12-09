cd5c48958f596a554a92b21e65dc325101a44c0f
tracer-ics2022 <tracer@njuics.org>
2022-12-09 15:10:57 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:10:56 up  2:50,  2 users,  load average: 1.92, 1.23, 0.70

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,7 +31,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   bool keydown = io_read(AM_INPUT_KEYBRD).keydown;
   cbuf[0] = 'k';
   cbuf[1] = (keydown) ? 'd' : 'u' ;
-
+  cbuf[2] = '\0';
   return len;
 }
 
