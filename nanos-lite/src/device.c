9d7c7775830f5ebc54aa67f2eb649b36dd47e6a4
tracer-ics2022 <tracer@njuics.org>
2022-12-09 15:14:20 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:14:20 up  2:53,  2 users,  load average: 0.93, 1.12, 0.77

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,13 +28,10 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
 
   assert(buf != NULL);
-  char* cbuf = (char *)buf;
   bool keydown = io_read(AM_INPUT_KEYBRD).keydown;
-  cbuf[0] = 'k';
-  cbuf[1] = (keydown) ? 'd' : 'u' ;
-  cbuf[2] = '\0';
+  sprintf((char*)buf, "kd %d", keydown);
   // printf("%d ",len);
-  return 2;
+  return len;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
