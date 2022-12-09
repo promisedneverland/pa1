167d77a607b8fdb4e3bf1d8fb2d903f5e733b8e4
tracer-ics2022 <tracer@njuics.org>
2022-12-09 15:56:42 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:56:42 up  3:35,  2 users,  load average: 0.92, 1.14, 1.16

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -8,7 +8,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   int cur_keycode;
   // if(kbd->keydown == 0)
   cur_keycode = inl(KBD_ADDR);//可能是通码也可能是断码
-  kbd->keydown = (cur_keycode & KEYDOWN_MASK) ? true : false;
+  kbd->keydown = (cur_keycode & KEYDOWN_MASK);
   kbd->keycode = cur_keycode & ~KEYDOWN_MASK;//取通码的部分
   // send_key(keycode);
   // kbd->keydown = (kbd->keycode) & KEYDOWN_MASK;
