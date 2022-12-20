eeb27d656a30a1143cd9ae2f1bcca54f6968af88
tracer-ics2022 <tracer@njuics.org>
2022-12-20 19:38:43 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:38:43 up  6:00,  3 users,  load average: 0.51, 0.58, 0.42

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -118,7 +118,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   // read();
   char* buf;
-  // sprintf(buf, "%d %d\n", w , h);
+  sprintf(buf, "%d %d\n", w , h);
   // write(fbctr, buf, strlen(buf));
   lseek(fbdev, (x + y * screen_w) , SEEK_SET);
   write(fbdev, pixels, sizeof(pixels));
