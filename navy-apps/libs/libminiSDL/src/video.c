4e24cd27d905d0887b43d8e0bc79327cdc846370
tracer-ics2022 <tracer@njuics.org>
2022-12-21 16:52:42 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:52:42 up  4:03,  5 users,  load average: 0.36, 0.31, 0.34

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -11,7 +11,10 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
-  assert(0);
+  for(int i = 0 ; i < dstrect->h; i++)
+  {
+    
+  }
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
