46c21040a17d4e233f849fe5aac73de763a284a4
tracer-ics2022 <tracer@njuics.org>
2022-12-21 16:10:42 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:10:42 up  3:21,  5 users,  load average: 0.58, 0.76, 0.81

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -25,8 +25,8 @@ void render() {
   }
   char fname[256];
   sprintf(fname, path, cur);
-  slide = SDL_LoadBMP(fname);
-  assert(slide);
+  // slide = SDL_LoadBMP(fname);
+  // assert(slide);
   // SDL_UpdateRect(slide, 0, 0, 0, 0);
 }
 
