5669ba2a5a8c99e8a6be39c81ed95d4d5683af9b
tracer-ics2022 <tracer@njuics.org>
2022-12-21 17:29:38 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:29:38 up  4:40,  6 users,  load average: 1.48, 0.98, 0.71

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -78,7 +78,7 @@ int main(int argc, char *argv[], char *envp[]) {
   
   while (1) {
     display_menu(i_max);
-    assert(0);
+    
     SDL_Event e;
     do {
       SDL_WaitEvent(&e);
@@ -143,6 +143,8 @@ static void display_menu(int n) {
   clear_display();
   SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
   SDL_BlitSurface(logo_sf, NULL, screen, &rect);
+
+  assert(0);
   printf("Available applications:\n");
   char buf[80];
   int i;
