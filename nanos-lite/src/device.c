c71d987174d810fed09da0d6188951e5e99fb430
tracer-ics2022 <tracer@njuics.org>
2022-12-21 16:02:38 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:02:38 up  3:13,  5 users,  load average: 1.49, 1.09, 0.85

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -29,30 +29,30 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 
 //len 一般为1024
 size_t events_read(void *buf, size_t offset, size_t len) {
-  printf("ok\n");
+  
   assert(buf != NULL);
   //绝对不能io_read().keycode这么用 
   // printf("%x\n",buf);
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  int keycode = ev.keycode;
-  if(keycode != AM_KEY_NONE)
-  {
-    // printf("press\n");
-    bool keydown = ev.keydown;
-    // printf("code %x keydown %d\n",keycode,keydown);
-    char ckeydown = (keydown) ? 'd' : 'u';
-    // char* cbuf = buf;
-    // int length;
-    // printf("k%c%s", ckeydown, keyname[keycode]);
-    // sprintf((char*)buf, "k%c %s", ckeydown, keyname[keycode]);
-    sprintf((char*)buf, "k%c %s", ckeydown, keycode);
-    // printf("%s, strlen = %d\n",buf,strlen(buf));
-    // sprintf((char*)buf + length, "asdasdasd");
-    // length = sprintf((char*)buf + length + 1, "%s");
+  // int keycode = ev.keycode;
+  // if(keycode != AM_KEY_NONE)
+  // {
+  //   // printf("press\n");
+  //   bool keydown = ev.keydown;
+  //   // printf("code %x keydown %d\n",keycode,keydown);
+  //   char ckeydown = (keydown) ? 'd' : 'u';
+  //   // char* cbuf = buf;
+  //   // int length;
+  //   // printf("k%c%s", ckeydown, keyname[keycode]);
+  //   // sprintf((char*)buf, "k%c %s", ckeydown, keyname[keycode]);
+  //   sprintf((char*)buf, "k%c %s", ckeydown, keycode);
+  //   // printf("%s, strlen = %d\n",buf,strlen(buf));
+  //   // sprintf((char*)buf + length, "asdasdasd");
+  //   // length = sprintf((char*)buf + length + 1, "%s");
    
-  }
-  else 
-    sprintf((char*)buf, "\0");
+  // }
+  // else 
+  //   sprintf((char*)buf, "\0");
   // printf("buf = %s\n",buf);
   // printf("%d ",len);
   return len;
