6ac1146deb7613b56b30450ed9a0f6a9c3a8c30d
tracer-ics2022 <tracer@njuics.org>
2022-12-21 17:18:28 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:18:28 up  4:28,  5 users,  load average: 1.15, 0.74, 0.54

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -28,7 +28,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
     {
       for(int x = dstrect->x; x < dstrect->x + dstrect->w; x++)
       {
-        dst->pixels[4 * (x + y * dst->w)    ] = (color & DEFAULT_AMASK >> maskToShift(DEFAULT_AMASK));
+        // dst->pixels[4 * (x + y * dst->w)    ] = (color & DEFAULT_AMASK >> maskToShift(DEFAULT_AMASK));
         // dst->pixels[4 * (x + y * dst->w) + 1] = (color & DEFAULT_RMASK >> maskToShift(DEFAULT_RMASK));
         // dst->pixels[4 * (x + y * dst->w) + 2] = (color & DEFAULT_GMASK >> maskToShift(DEFAULT_GMASK));
         // dst->pixels[4 * (x + y * dst->w) + 3] = (color & DEFAULT_BMASK >> maskToShift(DEFAULT_BMASK));
