b0cb30ab5e92216e991ad23e0f8e48f7ea4ecb02
tracer-ics2022 <tracer@njuics.org>
2022-12-21 16:08:33 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:08:33 up  3:18,  5 users,  load average: 0.84, 0.93, 0.86

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -114,7 +114,7 @@ size_t fs_write(int fd, const void *buf, size_t len)
     return file_table[fd].write(buf,file_table[fd].open_offset,len);
   
   int ret = 0;
-  // printf("fs_write fd = %d , open_offset = %x, len = %d\n",fd,file_table[fd].open_offset,len);
+  printf("fs_write fd = %d , open_offset = %x, len = %d\n",fd,file_table[fd].open_offset,len);
   //文件偏移量超过了文件大小
   if(file_table[fd].open_offset >= file_table[fd].size)
   {
