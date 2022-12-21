a23754710b435927be85b976c7f7b4e1642c63bd
tracer-ics2022 <tracer@njuics.org>
2022-12-21 21:40:55 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:40:55 up  8:51,  6 users,  load average: 2.51, 1.79, 1.15

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -32,15 +32,15 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     {
       for(int x = 0; x < srcrect->w ; x++)
       {
-        // dst->pixels[4 * (dstrect->x + x + srcrect->w * (dstrect->y + y)) + 0] = src->pixels[4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 0];
-        // dst->pixels[4 * (dstrect->x + x + srcrect->w * (dstrect->y + y)) + 1] = src->pixels[4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 1];
-        // dst->pixels[4 * (dstrect->x + x + srcrect->w * (dstrect->y + y)) + 2] = src->pixels[4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 2];
-        // dst->pixels[4 * (dstrect->x + x + srcrect->w * (dstrect->y + y)) + 3] = src->pixels[4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 3];
+        dst->pixels[4 * ( x + srcrect->w * (y)) + 0] = src->pixels[4 * (x + srcrect->w * (y)) + 0];
+        dst->pixels[4 * ( x + srcrect->w * (y)) + 1] = src->pixels[4 * (x + srcrect->w * (y)) + 1];
+        dst->pixels[4 * ( x + srcrect->w * (y)) + 2] = src->pixels[4 * (x + srcrect->w * (y)) + 2];
+        dst->pixels[4 * ( x + srcrect->w * (y)) + 3] = src->pixels[4 * (x + srcrect->w * (y)) + 3];
 
-        dst->pixels[4 * ( x + srcrect->w * ( y)) + 0] = 0x00;
-        dst->pixels[4 * ( x + srcrect->w * (y)) + 1] = 0x80;
-        dst->pixels[4 * ( x + srcrect->w * ( y)) + 2] = 0x80;
-        dst->pixels[4 * ( x + srcrect->w * ( y)) + 3] = 0xff;
+        // dst->pixels[4 * ( x + srcrect->w * ( y)) + 0] = 0x00;
+        // dst->pixels[4 * ( x + srcrect->w * (y)) + 1] = 0x80;
+        // dst->pixels[4 * ( x + srcrect->w * ( y)) + 2] = 0x80;
+        // dst->pixels[4 * ( x + srcrect->w * ( y)) + 3] = 0xff;
         // dst->pixels[4* (x + y * srcrect->w) + 0] = 0x00;//b
         // dst->pixels[4* (x + y * srcrect->w) + 1] = 0x80;//G
         // dst->pixels[4* (x + y * srcrect->w) + 2] = 0x80;//R
