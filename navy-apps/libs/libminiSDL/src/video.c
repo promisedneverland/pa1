195eb4a96c65d57c440391b41ae895f3b4b0afa4
tracer-ics2022 <tracer@njuics.org>
2022-12-21 18:52:15 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:52:15 up  6:02,  6 users,  load average: 0.56, 0.68, 0.44

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -44,7 +44,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
     for(int x = dstrect->x; x < dstrect->x + dstrect->w; x++)
     {
       // dst->pixels[4 * (x + y * dst->w)    ] = (uint8_t) (0xFF);
-      dst->pixels[4 * (x + y * dst->w) + 0] = (uint8_t) ((color & DEFAULT_AMASK) >> maskToShift(DEFAULT_AMASK));
+      // dst->pixels[4 * (x + y * dst->w) + 0] = (uint8_t) ((color & DEFAULT_AMASK) >> maskToShift(DEFAULT_AMASK));
       // dst->pixels[4 * (x + y * dst->w) + 1] = (uint8_t) ((color & DEFAULT_RMASK) >> maskToShift(DEFAULT_RMASK));
       // dst->pixels[4 * (x + y * dst->w) + 2] = (uint8_t) ((color & DEFAULT_GMASK) >> maskToShift(DEFAULT_GMASK));
       // dst->pixels[4 * (x + y * dst->w) + 3] = (uint8_t) ((color & DEFAULT_BMASK) >> maskToShift(DEFAULT_BMASK));
