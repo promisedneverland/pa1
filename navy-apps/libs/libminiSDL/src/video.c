53d51cc85ab87128443c0e60662cc4ad08d00a43
tracer-ics2022 <tracer@njuics.org>
2022-12-22 11:50:14 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:50:14 up  1:20,  5 users,  load average: 0.52, 0.72, 0.69

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -93,6 +93,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 //提供的是surface的pixel
 uint8_t tmp[300 * 400 * 4];
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+  printf("updaterect\n");
   NDL_OpenCanvas(&w,&h);
   if(w == 0 && h == 0)
   {
