7e1b297958d749aace2ea198104344b0f9ea16a9
tracer-ics2022 <tracer@njuics.org>
2022-12-22 13:18:54 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:18:54 up  2:49,  5 users,  load average: 1.18, 1.16, 1.07

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -9,6 +9,7 @@ int main() {
   int w, h;
   void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
   assert(bmp);
+  printf("bmp w = %d, h = %d\n",w,h);
   w = 0;
   h = 0;
   NDL_OpenCanvas(&w, &h);
