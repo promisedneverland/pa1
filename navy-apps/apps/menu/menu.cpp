486b1b3b974a28584466f02b26977963a7f379b3
tracer-ics2022 <tracer@njuics.org>
2022-12-22 13:40:45 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:40:45 up  3:11,  5 users,  load average: 1.16, 1.07, 1.07

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -212,7 +212,7 @@ static void display_menu(int n) {
       }
     }
   }
-
+  while(1){;}
   SDL_UpdateRect(screen, 0, 0, 0, 0);
   // NDL_DrawRect(screen->pixels, 0, 0, screen->w, screen->h);
   printf("========================================\n");
