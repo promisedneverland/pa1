646c4105c78cd7177dad00721ebaae9e83095bbf
tracer-ics2022 <tracer@njuics.org>
2022-12-22 12:28:51 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:28:51 up  1:59,  5 users,  load average: 0.60, 0.44, 0.43

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -41,14 +41,14 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
         dst->pixels[4 * ( dstrect->x + x + srcrect->w * (dstrect->y + y)) + 2] = src->pixels[4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 2];
         dst->pixels[4 * ( dstrect->x + x + srcrect->w * (dstrect->y + y)) + 3] = src->pixels[4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 3];
       
-        dst->pixels[4* (x + y * srcrect->w) + 0] = 0x00;//b
-        dst->pixels[4* (x + y * srcrect->w) + 1] = 0x80;//G
-        dst->pixels[4* (x + y * srcrect->w) + 2] = 0x80;//R
-        dst->pixels[4* (x + y * srcrect->w) + 3] = 0xff;//A
+        // dst->pixels[4* (x + y * srcrect->w) + 0] = 0x00;//b
+        // dst->pixels[4* (x + y * srcrect->w) + 1] = 0x80;//G
+        // dst->pixels[4* (x + y * srcrect->w) + 2] = 0x80;//R
+        // dst->pixels[4* (x + y * srcrect->w) + 3] = 0xff;//A
       }
     }
   // assert(0);
-  printf("xd = %d,y = %d, w = %d , h = %d\n",dstrect->x,dstrect->y,srcrect->w,srcrect->h);
+  printf("blit x = %d,y = %d, w = %d , h = %d\n",dstrect->x,dstrect->y,srcrect->w,srcrect->h);
   // SDL_UpdateRect(dst, dstrect->x, dstrect->y, srcrect->w, srcrect->h);
   SDL_UpdateRect(dst,0,0,0,0);
   // while(1)
