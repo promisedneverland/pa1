089201ab72ef3515470bdcccc74419817c2f5758
tracer-ics2022 <tracer@njuics.org>
2022-12-22 12:22:35 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:22:35 up  1:53,  5 users,  load average: 1.16, 0.55, 0.47

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -49,7 +49,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     }
   // assert(0);
   // printf("xd = %d,y = %d, w = %d , h = %d\n",dstrect->x,dstrect->y,srcrect->w,srcrect->h);
-  SDL_UpdateRect(dst, dstrect->x, dstrect->y, srcrect->w, srcrect->h);
+  // SDL_UpdateRect(dst, dstrect->x, dstrect->y, srcrect->w, srcrect->h);
+  SDL_UpdateRect(dst,0,0,0,0);
   // while(1)
   // {;}
 }
