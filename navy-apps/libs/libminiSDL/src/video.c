542caf2eb571c53f76119e5f92f604232ab49c1c
tracer-ics2022 <tracer@njuics.org>
2022-12-22 18:37:32 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:37:32 up  1:50,  4 users,  load average: 1.27, 1.02, 0.79

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -39,22 +39,22 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   // printf("blit x = %d,y = %d, w = %d , h = %d\n",dstrect->x,dstrect->y,srcrect->w,srcrect->h);
 
   //8-bit
-    // for(int y = 0; y < srcrect->h ; y++)
-    // {
-    //   for(int x = 0; x < srcrect->w ; x++)
-    //   {
+    for(int y = 0; y < srcrect->h ; y++)
+    {
+      for(int x = 0; x < srcrect->w ; x++)
+      {
         
-    //     // printf("offset = %d\n",4 * ( dstrect->x + x + dst->w * (dstrect->y + y)) + 0 );
-    //     // printf("offsets = %d\n",4 * 4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 0 );
-    //     dst->pixels[( dstrect->x + x + dst->w * (dstrect->y + y))] = src->pixels[(srcrect->x + x + srcrect->w * (srcrect->y + y))];
+        // printf("offset = %d\n",4 * ( dstrect->x + x + dst->w * (dstrect->y + y)) + 0 );
+        // printf("offsets = %d\n",4 * 4 * (srcrect->x + x + srcrect->w * (srcrect->y + y)) + 0 );
+        dst->pixels[( dstrect->x + x + dst->w * (dstrect->y + y))] = src->pixels[(srcrect->x + x + srcrect->w * (srcrect->y + y))];
 
       
-    //     // dst->pixels[4* (x + y * srcrect->w) + 0] = 0x00;//b
-    //     // dst->pixels[4* (x + y * srcrect->w) + 1] = 0x80;//G
-    //     // dst->pixels[4* (x + y * srcrect->w) + 2] = 0x80;//R
-    //     // dst->pixels[4* (x + y * srcrect->w) + 3] = 0xff;//A
-    //   }
-    // }
+        // dst->pixels[4* (x + y * srcrect->w) + 0] = 0x00;//b
+        // dst->pixels[4* (x + y * srcrect->w) + 1] = 0x80;//G
+        // dst->pixels[4* (x + y * srcrect->w) + 2] = 0x80;//R
+        // dst->pixels[4* (x + y * srcrect->w) + 3] = 0xff;//A
+      }
+    }
   // for(int y = 0; y < srcrect->h ; y++)
   //   {
   //     for(int x = 0; x < srcrect->w ; x++)
@@ -98,10 +98,10 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
     {
       // dst->pixels[4 * (x + y * dst->w)    ] = (uint8_t) (0xFF);
       // printf("fisrt = %x\n",dst->pixels[4 * (x + y * dst->w) + 0]);
-      dst->pixels[4 * (x + y * dst->w) + 0] = (uint8_t) ((color & DEFAULT_BMASK) >> maskToShift(DEFAULT_BMASK));
-      dst->pixels[4 * (x + y * dst->w) + 1] = (uint8_t) ((color & DEFAULT_GMASK) >> maskToShift(DEFAULT_GMASK));
-      dst->pixels[4 * (x + y * dst->w) + 2] = (uint8_t) ((color & DEFAULT_RMASK) >> maskToShift(DEFAULT_RMASK));
-      dst->pixels[4 * (x + y * dst->w) + 3] = (uint8_t) ((color & DEFAULT_AMASK) >> maskToShift(DEFAULT_AMASK));
+      // dst->pixels[4 * (x + y * dst->w) + 0] = (uint8_t) ((color & DEFAULT_BMASK) >> maskToShift(DEFAULT_BMASK));
+      // dst->pixels[4 * (x + y * dst->w) + 1] = (uint8_t) ((color & DEFAULT_GMASK) >> maskToShift(DEFAULT_GMASK));
+      // dst->pixels[4 * (x + y * dst->w) + 2] = (uint8_t) ((color & DEFAULT_RMASK) >> maskToShift(DEFAULT_RMASK));
+      // dst->pixels[4 * (x + y * dst->w) + 3] = (uint8_t) ((color & DEFAULT_AMASK) >> maskToShift(DEFAULT_AMASK));
 
       // dst->pixels[4* (x + y * dst->w) + 0] = 0x00;//b
       // dst->pixels[4* (x + y * dst->w) + 1] = 0x80;//G
