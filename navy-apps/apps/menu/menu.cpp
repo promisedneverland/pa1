d32530ef647233d1ab99b0ed62d925d43c8727ba
tracer-ics2022 <tracer@njuics.org>
2022-12-22 13:33:59 +0800

    >  compile NEMU 211240022 李成蹊 Linux linux 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:33:59 up  3:04,  5 users,  load average: 1.06, 1.11, 1.08

--- navy-apps/apps/menu/menu.cpp
+++ navy-apps/apps/menu/menu.cpp
@@ -66,7 +66,10 @@ static void prev() {
 static void clear_display(void) {
   // printf("start\n");
   // assert(0);
+  
   SDL_FillRect(screen, NULL, 0xffffff);
+  printf("screen w = %d, h = %d\n",screen->w,screen->h);
+  while(1) {;}
   for(int y = 0 ; y < screen->h; y++)
   {
     for(int x = 0 ; x < screen -> w; x++)
